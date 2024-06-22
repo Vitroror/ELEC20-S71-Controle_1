55e48a11ccf97e78c0fbd39f07b7efcaae282f07
VM <82391099+Vitroror@users.noreply.github.com>
2024-06-22 16:46:15 -0300

    phase delay controler

--- teste.m
+++ teste.m
@@ -2,10 +2,17 @@ clear all
 close all
 clc
 
-%Questão 1: Análise de Malha Aberta
-% declarando o sistema
+%TESTE: Controlador de atraso de fase
 s = tf('s');
-sys = (2)/(s*(s+2));
-K = 10;
+sys = (10*5)/(s*(s+5)*(0.1*s+5));
+K = 11;
+phi = 50 + 7;
 zpk(sys)
 margin(K*sys)
+alph = 10^(15.7/20);
+tau = 1/(0.1*2.85);
+D = K * (tau*s+1)/(alph*tau*s+1);
+margin(D*sys)
+figure(2)
+step(feedback(D*sys,1))
+stepinfo(feedback(D*sys,1))
